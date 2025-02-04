43e262c866220279d8c203ab9fd3566c4868e443
Boris-Picard <picard.boris@gmail.com>
2025-02-04 23:02:05 +0100

    fix : tailwind & import fixed

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import './index.css'
+import LoginPage from "./login/page.tsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <LoginPage />
     </>
   )
 }
