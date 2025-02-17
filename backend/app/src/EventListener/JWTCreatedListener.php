99d91cc853cde1ecf7b72be3220f7386cfa17c2c
Boris-Picard <picard.boris@gmail.com>
2025-02-17 21:02:16 +0100

    feature: added listener to surchage jwt payload

---
+++ backend/app/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Utilisateur;
+use App\Repository\UtilisateurRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+    public function __construct(
+        protected RequestStack $requestStack,
+        protected UtilisateurRepository $userRepository
+    ) {}
+
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        $payload = $event->getData();
+
+        if (!isset($payload['username'])) {
+            return;
+        }
+        $user = $this->userRepository->findOneBy(['ut_mail' => $payload['username']]);
+
+        if (!$user instanceof Utilisateur) {
+            return;
+        }
+
+        $payload['firstname'] = $user->getUtPrenom();
+        $payload['lastname'] = $user->getUtNom();
+
+        $event->setData($payload);
+    }
+}
