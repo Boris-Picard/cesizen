0fb7d5ec12d9e8f8357d9bb3442d6176a9d71324
Boris-Picard <picard.boris@gmail.com>
2025-04-12 12:19:22 +0200

    fix : multiple fix entrypoint & dockerfile & nginx

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -2,35 +2,15 @@
 
 set -e
 
-# Petite fonction pour vérifier si un package est dans le composer.json
-function has_composer_package() {
-    local package="$1"
-    composer show "$package" > /dev/null 2>&1 && return 0 || return 1
-}
-
-function init_symfony() {
-    echo "Initialisation d'un nouveau projet Symfony..."
-    composer create-project symfony/skeleton . --no-interaction
-    composer require api
-    composer require doctrine
-    composer require nelmio/cors-bundle
-}
-
-# 1. Si pas de composer.json, potentiellement dossier vide => init
-if [ ! -f "/var/www/composer.json" ]; then
-    if [ -z "$(ls -A /var/www)" ]; then
-        init_symfony
-    else
-        echo "Le dossier /var/www n'est pas vide mais aucun composer.json trouvé. On ne fait rien."
-    fi
-else
-    echo "composer.json trouvé, pas d'initialisation du projet Symfony."
-    echo "Les dépendances Composer sont déjà installées dans l'image. Rien à faire."
-fi
-
-# Attendre Postgres
 echo "En attente de PostgreSQL..."
 wait-for-it.sh db:5432 --timeout=60 -- echo "PostgreSQL est prêt."
 
-# Lancer la commande finale du Dockerfile
-exec "$@"
+# Clear & warm cache
+php bin/console cache:clear --env=prod
+php bin/console cache:warmup --env=prod
+
+# Lancer PHP-FPM en arrière-plan
+php-fpm -D
+
+# Lancer NGINX
+exec nginx -g 'daemon off;'
