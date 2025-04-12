841f36d409239347d4e4d5a59046b509198d19aa
Boris-Picard <picard.boris@gmail.com>
2025-04-12 08:52:54 +0200

    fix : index env

--- backend/app/public/index.php
+++ backend/app/public/index.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+
+$envFile = dirname(__DIR__).'/.env';
+
+if (!isset($_ENV['APP_ENV']) && file_exists($envFile)) {
+    (new Dotenv())->bootEnv($envFile);
+}
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'] ?? 'prod', (bool) ($context['APP_DEBUG'] ?? false));
 };
