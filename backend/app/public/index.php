794403818f1bdd8a9d7092f2e7197be2bd758cbe
Boris-Picard <picard.boris@gmail.com>
2025-04-12 09:06:10 +0200

    fix : prod

--- backend/app/public/index.php
+++ backend/app/public/index.php
@@ -1,24 +1,9 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-
-// Forcer la lecture de .env uniquement si aucune variable d'env n'est définie
-if (!isset($_ENV['APP_ENV']) && file_exists(dirname(__DIR__).'/.env')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-} else {
-    // Render ou Docker ont fourni les variables d'env
-    $_SERVER['APP_ENV'] = $_SERVER['APP_ENV'] ?? 'prod';
-    $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? '0';
-    $_ENV['APP_ENV'] = $_ENV['APP_ENV'] ?? 'prod';
-    $_ENV['APP_DEBUG'] = $_ENV['APP_DEBUG'] ?? '0';
-}
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel(
-        $context['APP_ENV'] ?? $_ENV['APP_ENV'],
-        (bool) ($context['APP_DEBUG'] ?? $_ENV['APP_DEBUG'])
-    );
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
